98adf862add4a4c5c9c50668a864d3a77c196573
PeterBytesTB <pedropowerbyte@gmail.com>
2025-09-09 22:03:29 -0300

    espero que funcione

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import CurrencyConverter from "./components/CurrencyConverter";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CurrencyConverter />
+    <App />
   </StrictMode>
 );
