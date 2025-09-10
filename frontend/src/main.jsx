79d72deddb2c2174d6c2918a8385eb367cc8fd74
PeterBytesTB <pedropowerbyte@gmail.com>
2025-09-09 21:58:18 -0300

    path correct now

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import CurrencyConverter from "./CurrencyConverter.jsx";
+import CurrencyConverter from "./components/CurrencyConverter";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
